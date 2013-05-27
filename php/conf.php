4873ed9906572f174428ef2287793161635c1169
KeithDoyle <kdoyle@immap.org>
2013-05-27 12:23:12 +0000

    Update conf.php

--- php/conf.php
+++ php/conf.php
@@ -4,7 +4,7 @@ $GLOBALS['dbname']="DBNAME";
 $GLOBALS['username']="USER";
 $GLOBALS['password']="PASS";
 $GLOBALS['port']="PORT";
-$GLOBALS['map_path']="c:/ms4w/apache/htdocs/oasisweb/mapfile/";
+$GLOBALS['map_path']="/var/www/oasiswebmapping/syria/mapfile/";
 $GLOBALS['MSSQLServerConn']="MSSQL:driver=FreeTDS;server=SERVERNAME,PORT;database=DBNAME;uid=USER;pwd=PASS;tables=TABLENAME(wkb)"
 
 ?>
