7ad9d6c0fe9b90365a9f9aa11538fd4ad538eee4
KeithDoyle <kdoyle@immap.org>
2013-06-04 13:52:50 +0000

    support for second postgis database

--- php/conf.php
+++ php/conf.php
@@ -7,4 +7,10 @@ $GLOBALS['port']="PORT";
 $GLOBALS['map_path']="/var/www/oasiswebmapping/syria/mapfile/";
 $GLOBALS['MSSQLServerConn']="MSSQL:driver=FreeTDS;server=SERVERNAME,PORT;database=DBNAME;uid=USER;pwd=PASS;tables=TABLENAME(wkb)"
 
+$GLOBALS['serverUrl2']="SERVERNAME";
+$GLOBALS['dbname2']="DBNAME";
+$GLOBALS['username2']="USER";
+$GLOBALS['password2']="PASS";
+$GLOBALS['port2']="PORT";
+
 ?>
