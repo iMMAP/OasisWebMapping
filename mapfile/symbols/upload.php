763043dec560429b8097cc367b1999b2370f0227
Keith Doyle <kdoyle@immap.org>
2013-06-14 15:52:32 +0000

    Upload scripts

---
+++ mapfile/symbols/upload.php
@@ -0,0 +1,20 @@
+<table width="500" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr>
+<form action="upload_ac.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
+<td>
+<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+<tr>
+<td><strong>Single File Upload </strong></td>
+</tr>
+<tr>
+<td>Select file 
+<input name="ufile" type="file" id="ufile" size="50" /></td>
+</tr>
+<tr>
+<td align="center"><input type="submit" name="Submit" value="Upload" /></td>
+</tr>
+</table>
+</td>
+</form>
+</tr>
+</table>
