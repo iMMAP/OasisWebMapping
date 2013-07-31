d3c4275080b184633df1a7d0f928b6a5c0c079e1
KeithDoyle <kdoyle@immap.org>
2013-07-31 07:59:38 +0100

    removed HEAD

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,10 +1,6 @@
 <?php
 function getDB() {
-<<<<<<< HEAD
 	return pg_connect("host=54.235.150.26 port=9191 dbname=oasisweb user=budi password=dw#\$df2dy@!Fs35S@");
-=======
-	return pg_connect("host=54.235.150.26 port=9191 dbname=oasisweb user=budi password=xxxxxxx");
->>>>>>> 0f3ba027ea74716e0ddf09cff351115031e7da5c
 }	
 function getSindhDB() {
 	return pg_connect("host=210.56.24.186 port=5432 dbname=incidents user=postgres password=xxxxx");
@@ -13,11 +9,7 @@ function getDevDB() {
 	return pg_connect("host=210.56.8.107 port=5432 dbname=odk_DIFD user=postgres password=xxxxx");
 }
 function getKDB() {
-<<<<<<< HEAD
 	return pg_connect("host=54.235.150.26 port=9191 dbname=oasisweb user=budi password=dw#\$df2dy@!Fs35S@");
-=======
-	return pg_connect("host=54.235.150.26 port=9191 dbname=oasisweb user=budi password=xxxxx");
->>>>>>> 0f3ba027ea74716e0ddf09cff351115031e7da5c
 }
 
 $GLOBALS['connectionstring'] = array(
