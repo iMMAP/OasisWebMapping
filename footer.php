f2cd6e004b35db61b9fd5e296c7d2b61ebbc84aa
KeithDoyle <kdoyle@immap.org>
2013-07-31 15:16:03 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,29 +1,3 @@
 <div class="footer">
-  <div class="footerinner">
-    <table width="980" border="0" align="left" cellpadding="0" cellspacing="0" class="partners">
-      <tbody>
-        <tr>
-          <td width="900" align="right" valign="top" style="width: 900px;">
-          <table width="900" border="0" align="right" cellpadding="0" cellspacing="0">
-            <tbody>
-              <tr>
-                <td valign="top" style="width: 200px; height: 80px;">
-                <div style="font-family: Century Gothic; font-weight: bold; font-size: 13px; color: #666666; margin: 0px 30px 0px 0px; width: 150px; float: right; text-align: right;">iMMAP PAKISTAN </div>
-                  <div style="font-family: Century Gothic; font-size: 12px; color: #666666; margin: 8px 0px 0px; width: 150px; float: right; text-align: left;"> Implementing Common Operating Picture for Disaster Response </div></td>
-                <td align="right" valign="top" style="width: 95px; height: 80px;"><img alt="" src="OASIS/images/immaplogo.png" width="102" height="88" /></td>
-              </tr>
-              <tr>
-                <td colspan="2" style="border-top-width: 2px; border-top-color: #cccccc; border-top-style: solid;">
-                <p style="font-family: Century Gothic; font-size: 12px; font-weight: normal; font-style: italic; color: #666666; padding: 10px 5px 0px; text-align: justify;"> iMMAP is a pioneering humanitarian organization that for more than a decade has led the way in the effective
-                  use of information management practices and principles in service to the world&rsquo;s most vulnerable populations.New approaches
-                  and new solutions are required to address a range of problems plaguing humanitarian.
-                  &nbsp; <a href="http://immap.org/" target="_blank">Read more.</a></p></td>
-              </tr>
-            </tbody>
-          </table></td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <div class="ce">� 2013 OASIS - Pakistan | <a href="http://www.facebook.com/immap.org" target="_blank">www.facebook.com/immap.org</a></div>
+  <div class="ce">� 2013 iMMAP| <a href="http://www.facebook.com/immap.org" target="_blank">www.facebook.com/immap.org</a></div>
 </div>
