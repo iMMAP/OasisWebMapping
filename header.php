509158d81f1a7b4c7466566ebbd3621d41869913
boedy1996 <boedy1996@gmail.com>
2013-08-01 18:11:13 +0500

    Update header.php

--- header.php
+++ header.php
@@ -1,33 +1,4 @@
 <div class="headerinner">
   <div class="oaisislogo"><a href="#"><img src="OASIS/images/oasislogo.png" width="220" height="80"></a></div>
   <div class="topbar"></div>
-  <div class="menu">
-  <div id='cssmenu'>
-<?php 
- $page_m = basename($_SERVER['PHP_SELF']);
-?>
-  <ul>
-  <?php 
-   if($page_m == 'login.php')
-   {
-   	?>
-   	   	<li class='active'><a href='login.php'><span>Home</span></a></li>
-   	 <?php 
-   	   	
-   }
-  else
-   {
-   	?>
-   	<li ><a href='login.php'><span>Home</span></a></li>
-   	<?php 
-
-   }
-   ?>
-</ul>
-
-
-</div>
-
-  
-  </div>
 </div>
